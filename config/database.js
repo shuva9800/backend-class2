104f6193071b39c002802122a6b4b9277400c1a5
shuva9800 <ffshuva@gmail.com>
2024-02-08 01:05:08 +0530

    error occure in post request

--- config/database.js
+++ config/database.js
@@ -2,13 +2,20 @@ const mongoose=require('mongoose');
 
 require("dotenv").config();
 
-const dbconnect = ()=>{
+// const dbconnect = () =>
+function dbconnect() {
     mongoose.connect(process.env.DATABASE_URL, {
         useNewUrlParser:true,
-        useUnifiedTopology:true
+        useUnifiedTopology:true,
     })
     .then(() => {console.log("Connection Successful")})
     .catch( (error) => {console.log("Recieved an error" + error)} );
 
 }
 
+// mongoose.connect('mongodb://127.0.0.1:27017/myDatabase', {
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// })
+// .then(() => {console.log("Connection Successful")})
+// .catch( (error) => {console.log("Recieved an error" + error)} );
--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const express= require('express');
-const app=express();
+const express = require('express');
+const app = express();
 
-app.listen(3000, ()=>{
-    console.log("server created successfully now you can write code here");
+//load config from env file
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+
+// need middlewrae to parse json request body
+
+app.use(express.json());
+
+//import routes for todo api
+const todoRoutes =require("./routes/todo");
+//mount the todo API routes
+app.use("api/v1",todoRoutes);
+
+//satrt server
+app.listen( port ,()=>{
+    console.log( `server started successfully at ${port}`)
+});
+
+//DB connection
+// const dbconnect = require("./config/database");
+// dbconnect();
+const mongoose= require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(() => {console.log("Connection Successful")})
+.catch( (error) => {console.log("Recieved an error" + error)} );
+
+
+
+
+
+
+
+app.get('/', (req, res)=>{
+ res.send("successfully exicuted default route ,home page")
 })
